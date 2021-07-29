2483d6baee01eb0cf9579fecb984608df643f3b9
yovanha <yfajardo0@ucol.mx>
2021-07-29 13:06:46 -0500

    Rename const To and From

--- FlightReservation/src/helpers/firestoreBookings.js
+++ FlightReservation/src/helpers/firestoreBookings.js
@@ -1,13 +1,12 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
 const registerFlight = async (route, navigation) => {
-  const {from, to, startDate, endDate, passengers} = route.params;
+  const {fromPlace, toPlace, startDate, endDate, passengers} = route.params;
   const newData = {
-    from,
-    to,
+    fromPlace,
+    toPlace,
     startDate,
     endDate,
     passengers,
@@ -19,15 +18,14 @@ const registerFlight = async (route, navigation) => {
     .then(async documentSnapshot => {
       if (documentSnapshot.exists) {
         var collectionData = documentSnapshot.data();
-        newData.id = (collectionData.flights.length + 1).toString();
+        newData.id = collectionData.flights.length.toString();
         collectionData.flights.push(newData);
-        console.log("COLECCIÓN DE VUELOS:",collectionData.flights);
         await firestore()
           .collection('bookings')
           .doc(auth().currentUser.uid)
           .set(collectionData);
       }
       navigation.navigate('Results');
-    })
+    });
 };
 export default registerFlight;
